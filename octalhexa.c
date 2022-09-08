2a733588765daae5bdf69868e5d651aca96e3370
HIND-HANSALI <hindhsl111@gmail.com>
2022-09-08 11:14:54 +0100

    add challenges

---
+++ octalhexa.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int nombre;
+printf("Entrez un nombre entier pour afficher sa valeur équivalente en octal et en hexadécimal");
+
+
+
+
+
+
+
+}
--- ordreinverse.c
+++ ordreinverse.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main(){
+    int nombre;
+    printf("entrer un nombre a 3 chiffres pour l'inverser :");
+    scanf("%d",&nombre);
+    printf("%d", nombre % 10);
+    nombre /= 10;
+    printf("%d", nombre % 10);
+    nombre /= 10;
+    printf("%d\n", nombre % 10);
 
     
 }
