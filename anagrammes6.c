e27f3ef27de5dba9e6b26522c150370d9ecb8419
HIND-HANSALI <hindhsl111@gmail.com>
2022-09-09 17:13:40 +0100

    add challenges

--- anagrammes6.c
+++ anagrammes6.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    char nom1[4],nom2[4],i,j,n;
+    char nom1[4],nom2[4],i,j,n=0;
     printf("entrerla 1ere chaine de caracteres contenant que A B C D :");
     scanf("%s",&nom1);
     printf("entrerla 2eme chaine de caracteres contenant que A B C D :");
     scanf("%s",&nom2);
-    for(i=0;i<4;i++){
+
+
+    for(i=0;i<4;i++){ 
         for(j=0;j<4;j++){
             if (nom1[i]== nom2[j]){
                 n++;
+                break;
             }
         }
         
